d7d7680981699a7b6b41f67acbb52e0bfdef1192
Sander van Beek <lakitna@gmail.com>
2018-06-05 11:53:02 +0200

    Changed display enviroment setting method

--- test/test_gui.py
+++ test/test_gui.py
@@ -1,11 +1,21 @@
 import os
+from subprocess import call
 import pytest
 from . import before
 import gui as module
 
+@pytest.yield_fixture(autouse=True)
+def run_around_tests():
+    # Code that will run before your test
+    os.environ["DISPLAY"] = ":0.0"
+
+    yield
+    # Code that will run after your test
+    # os.system("export DISPLAY=:0.0")
+
+
 @pytest.fixture
 def gui():
-    os.system("export DISPLAY=:0.0")
     return module.GUI()
 
 
@@ -13,3 +23,6 @@ def test_init():
     g = module.GUI(fps=10, fullscreen=True)
     assert g.loop_delay == 0.01
     assert g.fullscreen is True
+
+
+# /private/tmp/com.apple.launchd.YaLYyBbubK/org.macosforge.xquartz:0
